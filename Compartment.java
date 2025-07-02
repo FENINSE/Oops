cf8865b2f956d23ebc3f9ade00e7f6a69bf5dd73
FENINSE <lintofeninse9@gmail.com>
2025-07-02 14:42:44 +0530

    Create Compartment.java

---
+++ Compartment.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+
+abstract class Compartment {
+    public abstract String notice();
+}
+
+class FirstClass extends Compartment {
+    public String notice() {
+        return "First Class Compartment";
+    }
+}
+
+class Ladies extends Compartment {
+    public String notice() {
+        return "Ladies Compartment";
+    }
+}
+
+class General extends Compartment {
+    public String notice() {
+        return "General Compartment";
+    }
+}
+
+class Luggage extends Compartment {
+    public String notice() {
+        return "Luggage Compartment";
+    }
+}
+// Main class to test
+class TestCompartment {
+    public static void main(String[] args) {
+        Compartment[] compartments = new Compartment[10];
+        Random rand = new Random();
+
+        for (int i = 0; i < compartments.length; i++) {
+            int randomNum = rand.nextInt(4) + 1; // Generates 1 to 4
+            switch (randomNum) {
+                case 1:
+                    compartments[i] = new FirstClass();
+                    break;
+                case 2:
+                    compartments[i] = new Ladies();
+                    break;
+                case 3:
+                    compartments[i] = new General();
+                    break;
+                case 4:
+                    compartments[i] = new Luggage();
+                    break;
+            }
+        }
+        for (int i = 0; i < compartments.length; i++) {
+            System.out.println("Compartment " + (i + 1) + ": " + compartments[i].notice());
+        }
+    }
+}
